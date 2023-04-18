4dfbc585ca49597b6c1b891ea38e508525676d71
jmorales <jonathan.morales.jmr@gmail.com>
2023-04-17 22:57:17 -0400

    actualizacion archivo

--- S13/COMPROBACION/js/script.js
+++ S13/COMPROBACION/js/script.js
@@ -2,16 +2,17 @@
 var tabla = document.getElementById("tabla");
 var contenido = document.getElementById("contenido");
 
+
 //consumo de api con fetch
 fetch("https://digimon-api.vercel.app/api/digimon")
 .then(response => response.json())
 .then(data =>{
     //console.log(datos);
-    mostrartabla(data);//invocando la funcion mostrarbabla()
+    mostrarTabla(data);//invocando la funcion mostrarbabla()
 }).catch(error => console.log(error))
 
 //funcion para obtener los datos del digimon y mostrarlo en tabla con id de tabla y id de contenido
-function mostrartabla(response){
+function mostrarTabla(response){
     contenido.innerHTML = "";
     //recorrer datos obtenidos en la api
     //console.log(response)
@@ -24,6 +25,45 @@ function mostrartabla(response){
 
         console.log(temp.name);
         
+    }
+
+}
+
+
+//funcion para obtener datos de digimon y mostrarle en la div con id carta
+//utilizando metodo fetch y el id de busqueda en el input
+function mostrarCarta(){
+    //obtener el valor del input con el id de busqueda
+    let busqueda = document.getElementById("busqueda").value.tolowerCase();
+
+    //consumo de api con fetch
+    fetch(`https://digimon-api.vercel.app/api/digimon/name/${"busqueda"}`)
+    .then(response => response.json())
+    .then(data =>{
+        //console.log(datos);
+        construirCarta(data);//invocando la funcion carta()
+    }).catch(error => console.log(error))
+}
+
+function construirCarta(params){
+    //ocultar tabla y tbody de la tabla
+    document.getElementById("tabla").style.display = "none";
+    document.getElementById("contenido").style.display ="none";
+    tabla.innerHTML = "";
+    contenido.innerHTML = "";
+    carta.innerHTML = "";
+    for(let temp of params){
+        carta +=
+        `<div class="card col-sm-12 co-md-6 mx-auto" style="width: 18rem;">
+            <img src="${temp.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${temp.name}</h5>
+                <p class="card-text">${temp.level}</p>
+            </div>
+        </div>
+  
+        `
+
     }
 
 }
